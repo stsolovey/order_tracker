a5163733c0941ee5d3da0e3f863becc339c412b0
stsolovey <st.solovey@gmail.com>
2024-05-24 11:37:32 +0300

    order struct fixed

--- internal/models/order.go
+++ internal/models/order.go
@@ -14,4 +14,44 @@ type Order struct {
 	SMID              int       `json:"smId,omitempty"`
 	DateCreated       time.Time `json:"dateCreated"`
 	OOFShard          string    `json:"oofShard,omitempty"`
+	Delivery          Delivery  `json:"delivery"`
+	Payment           Payment   `json:"payment"`
+	Items             []Item    `json:"items"`
+}
+
+type Delivery struct {
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Zip     string `json:"zip,omitempty"`
+	City    string `json:"city"`
+	Address string `json:"address"`
+	Region  string `json:"region,omitempty"`
+	Email   string `json:"email,omitempty"`
+}
+
+type Payment struct {
+	Transaction  string    `json:"transaction"`
+	RequestID    string    `json:"requestId,omitempty"`
+	Currency     string    `json:"currency"`
+	Provider     string    `json:"provider"`
+	Amount       float64   `json:"amount"`
+	PaymentDT    time.Time `json:"paymentDt"`
+	Bank         string    `json:"bank,omitempty"`
+	DeliveryCost float64   `json:"deliveryCost,omitempty"`
+	GoodsTotal   float64   `json:"goodsTotal"`
+	CustomFee    float64   `json:"customFee"`
+}
+
+type Item struct {
+	ChrtID      int     `json:"chrtId"`
+	TrackNumber string  `json:"trackNumber"`
+	Price       float64 `json:"price"`
+	RID         string  `json:"rid,omitempty"`
+	Name        string  `json:"name"`
+	Sale        int     `json:"sale,omitempty"`
+	Size        string  `json:"size,omitempty"`
+	TotalPrice  float64 `json:"totalPrice"`
+	NMID        int     `json:"nmId"`
+	Brand       string  `json:"brand"`
+	Status      int     `json:"status"`
 }
