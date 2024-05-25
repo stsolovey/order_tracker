f4a8a2d7676b01d751a4f3c9c117910c58cdba98
stsolovey <st.solovey@gmail.com>
2024-05-25 13:07:41 +0300

    context signal changed, orderCache&app added

--- cmd/order_service/main.go
+++ cmd/order_service/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"os/signal"
+	"syscall"
 
 	_ "github.com/jackc/pgx/v5/stdlib" // Importing `pgx/v5/stdlib` is necessary for `sql.Open("pgx", s.dsn)`.
 	"github.com/stsolovey/order_tracker/internal/config"
 	"github.com/stsolovey/order_tracker/internal/logger"
+	ordercache "github.com/stsolovey/order_tracker/internal/order-cache"
+	"github.com/stsolovey/order_tracker/internal/service"
 	"github.com/stsolovey/order_tracker/internal/storage"
 )
 
@@ -13,7 +17,7 @@ func main() {
 	cfg := config.New("./.env")
 	log := logger.New(cfg.LogLevel)
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	defer cancel()
 
 	storageSystem, err := storage.NewStorage(ctx, cfg.DatabaseURL)
@@ -23,5 +27,12 @@ func main() {
 
 	if err := storageSystem.Migrate(log); err != nil {
 		log.WithError(err).Panic("Failed to execute migrations")
+	}
+
+	orderCache := ordercache.New(log)
+	app := service.New(log, orderCache, storageSystem)
+
+	if err := app.Init(ctx); err != nil {
+		log.WithError(err).Panic("Error app initialisation")
 	}
 }
