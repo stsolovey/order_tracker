90b16c3259b132ca63301bd893b660de04a1041b
stsolovey <st.solovey@gmail.com>
2024-05-26 10:59:37 +0300

    test reworked with test suite

--- go.mod
+++ go.mod
@@ -7,15 +7,19 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/rubenv/sql-migrate v1.6.1
 	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.8.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-gorp/gorp/v3 v3.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/order-cache/order_cache_test.go
+++ internal/order-cache/order_cache_test.go
@@ -2,20 +2,37 @@ package ordercache
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
 	"github.com/stsolovey/order_tracker/internal/models"
 )
 
-func TestOrderCache_Upsert(t *testing.T) {
-	log := logrus.New()
-	cache := New(log)
+type OrderCacheSuite struct {
+	suite.Suite
+	cache  *OrderCache
+	ctx    context.Context
+	cancel context.CancelFunc
+}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+func (s *OrderCacheSuite) SetupTest() { // run before
+	logger := logrus.New()
+	s.cache = New(logger)
+	s.ctx, s.cancel = context.WithCancel(context.Background())
+}
 
+func (s *OrderCacheSuite) TearDownTest() { // run after
+	s.cancel()
+}
+
+func TestOrderCacheSuite(t *testing.T) {
+	suite.Run(t, new(OrderCacheSuite))
+}
+
+func (s *OrderCacheSuite) TestUpsert() {
 	order := models.Order{
 		OrderUID:    "testUID123",
 		TrackNumber: "TN1234567890",
@@ -23,52 +40,31 @@ func TestOrderCache_Upsert(t *testing.T) {
 		DateCreated: time.Now(),
 	}
 
-	cache.Upsert(ctx, order)
-	if got, _ := cache.Get(ctx, order.OrderUID); got == nil {
-		t.Errorf("Upsert() failed to add order, want order UID %s, got nothing", order.OrderUID)
-	} else {
-		if got.OrderUID != order.OrderUID {
-			t.Errorf("Upsert() failed, want order UID %s, got %s", order.OrderUID, got.OrderUID)
-		}
-	}
+	s.Run("inserting a new order", func() {
+		err := s.cache.Upsert(s.ctx, order)
+		s.Require().NoError(err)
 
-	order.TrackNumber = "TN0987654321"
-	cache.Upsert(ctx, order)
-	if got, err := cache.Get(ctx, order.OrderUID); err != nil {
-		t.Errorf("Upsert() failed to update order, want track number %s, got error: %v", order.TrackNumber, err)
-	} else {
-		if got.TrackNumber != order.TrackNumber {
-			t.Errorf("Upsert() failed to update order, want track number %s, got %s",
-				order.TrackNumber, got.TrackNumber)
-		}
-	}
+		retrievedOrder, err := s.cache.Get(s.ctx, order.OrderUID)
+		s.Require().NoError(err, "Get should not return an error after Upsert")
+		s.Require().NotNil(retrievedOrder, "Retrieved order should not be nil")
+		s.Require().Equal(order.OrderUID, retrievedOrder.OrderUID)
+	})
+
+	updatedOrder := order
+	updatedOrder.TrackNumber = "TN0987654321"
+
+	s.Run("updating an existing order", func() {
+		err := s.cache.Upsert(s.ctx, updatedOrder)
+		s.Require().NoError(err)
+
+		retrievedUpdatedOrder, err := s.cache.Get(s.ctx, updatedOrder.OrderUID)
+		s.Require().NoError(err)
+		s.Require().NotNil(retrievedUpdatedOrder)
+		s.Require().Equal(updatedOrder.TrackNumber, retrievedUpdatedOrder.TrackNumber)
+	})
 }
 
-func TestOrderCache_Delete(t *testing.T) {
-	log := logrus.New()
-	cache := New(log)
-	orderUID := "testUID123"
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	cache.Upsert(ctx, models.Order{OrderUID: orderUID, CustomerID: "Cust123", DateCreated: time.Now()})
-
-	cache.Delete(ctx, orderUID)
-
-	got, err := cache.Get(ctx, orderUID)
-	if err != nil {
-		if err != models.ErrOrderNotFound {
-			t.Errorf("Delete() failed with an unexpected error: %v", err)
-		}
-	} else if got != nil {
-		t.Errorf("Delete() failed, order UID %s should have been deleted but was found", orderUID)
-	}
-}
-
-func TestOrderCache_Get(t *testing.T) {
-	log := logrus.New()
-	cache := New(log)
+func (s *OrderCacheSuite) TestGet() {
 	orderUID := "testUID456"
 	order := models.Order{
 		OrderUID:    orderUID,
@@ -77,16 +73,34 @@ func TestOrderCache_Get(t *testing.T) {
 		DateCreated: time.Now(),
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	err := s.cache.Upsert(s.ctx, order)
+	s.Require().NoError(err, "Upsert should not fail")
 
-	cache.Upsert(ctx, order)
+	s.Run("retrieve existing order", func() {
+		retrievedOrder, err := s.cache.Get(s.ctx, orderUID)
+		s.Require().NoError(err, "Error returned")
+		s.Require().NotNil(retrievedOrder, "Order is nil!")
+		s.Require().Equal(order.OrderUID, retrievedOrder.OrderUID, "Wrong OrderUID's!")
+	})
+}
 
-	if got, _ := cache.Get(ctx, orderUID); got == nil {
-		t.Errorf("Get() failed, order UID %s should have been retrieved but was not found", orderUID)
-	} else {
-		if got.OrderUID != order.OrderUID {
-			t.Errorf("Get() failed, want order UID %s, got %s", order.OrderUID, got.OrderUID)
-		}
+func (s *OrderCacheSuite) TestDelete() {
+	orderUID := "testUID123"
+	order := models.Order{
+		OrderUID:    orderUID,
+		CustomerID:  "Cust123",
+		DateCreated: time.Now(),
 	}
+
+	err := s.cache.Upsert(s.ctx, order)
+	s.Require().NoError(err, "Upsert should not fail before delete")
+
+	s.cache.Delete(s.ctx, orderUID)
+
+	retrievedOrder, err := s.cache.Get(s.ctx, orderUID)
+	s.Run("verify deletion", func() {
+		s.Require().Error(err, "Get should return an error after Delete")
+		s.Require().True(errors.Is(err, models.ErrOrderNotFound), "Error should be 'ErrOrderNotFound'")
+		s.Require().Nil(retrievedOrder, "Retrieved order should be nil after deletion")
+	})
 }
