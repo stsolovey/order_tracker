4f715507023c7784fa80657c675a8d5ceb59d0cd
stsolovey <st.solovey@gmail.com>
2024-05-30 17:59:55 +0300

    Handle async problems in test

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -75,18 +75,20 @@ func (s *IntegrationTestSuite) TestNATSIntegration() {
 		ticker := time.NewTicker(250 * time.Millisecond)
 		defer ticker.Stop()
 
-		for {
+		found := false
+		for !found {
 			select {
 			case <-timeout:
 				s.Require().Fail("timeout waiting for the order to be stored")
+				return
 			case <-ticker.C:
 				fetchedOrder, err = s.db.Get(context.Background(), order.OrderUID)
 				if err == nil && fetchedOrder != nil {
-					goto Found
+					found = true
 				}
 			}
 		}
-	Found:
+
 		s.Require().NoError(err, "should fetch order from the database without error")
 		s.Require().NotNil(fetchedOrder, "fetched order should not be nil")
 	})
@@ -100,18 +102,20 @@ func (s *IntegrationTestSuite) TestNATSIntegration() {
 		ticker := time.NewTicker(250 * time.Millisecond)
 		defer ticker.Stop()
 
-		for {
+		found := false
+		for !found {
 			select {
 			case <-timeout:
 				s.Require().Fail("timeout waiting for the order to be stored")
+				return
 			case <-ticker.C:
 				cachedOrder, err = s.orderCache.Get(context.Background(), order.OrderUID)
 				if err == nil && cachedOrder != nil {
-					goto Found
+					found = true
 				}
 			}
 		}
-	Found:
+
 		s.Require().NoError(err, "should fetch order from the cache without error")
 		s.Require().NotNil(cachedOrder, "cached order should not be nil")
 	})
@@ -125,18 +129,20 @@ func (s *IntegrationTestSuite) TestNATSIntegration() {
 		ticker := time.NewTicker(250 * time.Millisecond)
 		defer ticker.Stop()
 
-		for {
+		found := false
+		for !found {
 			select {
 			case <-timeout:
 				s.Require().Fail("timeout waiting for the order to be stored")
+				return
 			case <-ticker.C:
 				retrievedOrder, err = s.app.GetOrder(context.Background(), order.OrderUID)
 				if err == nil && retrievedOrder != nil {
-					goto Found
+					found = true
 				}
 			}
 		}
-	Found:
+
 		s.Require().NoError(err, "should GetOrder(...) work without error")
 		s.Require().NotNil(retrievedOrder, "retrieved with GetOrder(...) should not be nil")
 		s.Require().Equal(order.OrderUID, retrievedOrder.OrderUID, "sent and got OrderUID should match")
@@ -147,8 +153,30 @@ func (s *IntegrationTestSuite) TestNATSIntegration() {
 	})
 
 	s.Run("Successful httpServer retrieving", func() {
+		err := s.natsClient.PublishOrder(order)
+		s.Require().NoError(err, "should publish without error")
+
+		var resp *http.Response
+		timeout := time.After(30 * time.Second)
+		ticker := time.NewTicker(250 * time.Millisecond)
+		defer ticker.Stop()
+
 		url := "http://localhost:" + s.cfg.AppPort + "/api/v1/orders/" + order.OrderUID
-		resp, err := http.Get(url)
+
+		found := false
+		for !found {
+			select {
+			case <-timeout:
+				s.Require().Fail("timeout waiting for the response to be get")
+				return
+			case <-ticker.C:
+				resp, err = http.Get(url)
+				if err == nil && resp != nil {
+					found = true
+				}
+			}
+		}
+
 		s.Require().NoError(err, "should fetch order through HTTP server without error")
 		defer func(Body io.ReadCloser) {
 			err := Body.Close()
@@ -161,6 +189,7 @@ func (s *IntegrationTestSuite) TestNATSIntegration() {
 
 		var httpOrder models.Order
 		err = json.NewDecoder(resp.Body).Decode(&httpOrder)
+
 		s.Require().NoError(err, "should decode HTTP response without error")
 		s.Require().Equal(order.OrderUID, httpOrder.OrderUID, "sent and got OrderUID should match")
 		s.Require().Equal(order.TrackNumber, httpOrder.TrackNumber, "sent and got TrackNumber should match")
